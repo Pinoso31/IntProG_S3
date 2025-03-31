e490822c67507e906030e71d321f3fadf9b38dfd
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-03-31 12:23:03 -0600

    ejemplo 1 semana 3

---
+++ Ejemplo1.py
@@ -0,0 +1,5 @@
+sistematico1 = int(input("ingrese la primer nota del primer sistematico (max: 30): "))
+sistematico2 = int(input("ingrese la nota del segundo sistematico (max : 30): "))
+sistematico3 = int(input("ingrese la nota del tercer sistematico (max : 40) : "))
+nota = (sistematico1 + sistematico2 + sistematico3)
+print("la nota del estudiante es: ", nota)
