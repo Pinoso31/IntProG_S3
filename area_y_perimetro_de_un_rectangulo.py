0b217d4699e1699fb72861fb7e3826e9a6cfaaac
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-04-02 13:56:32 -0600

    Ejercicio 3 semana 3

---
+++ area_y_perimetro_de_un_rectangulo.py
@@ -0,0 +1,6 @@
+base = float(input("introduce la base del rectangulo"))
+altura = float(input("introduce la altura del rectangulo"))
+area = base * altura
+perimetro = (base*2) + (altura*2)
+print("el area del perimetro: ", area)
+print("el perimetro del rectangulo es: ", perimetro)
