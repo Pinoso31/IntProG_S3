665fc3017adea6b2cfbd63cecae1302700a61fd7
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-04-02 14:22:22 -0600

    Ejercicio 4 semana 3

---
+++ calculo_salario_neto.py
@@ -0,0 +1,11 @@
+salrbrut = int(input("Ingrese el salario neto del empleado"))
+impuesto = salrbrut*0.1
+segr_social = salrbrut*0.5
+fondo_pensum = salrbrut*0.3
+desctotal =(impuesto+segr_social+fondo_pensum)
+print("**********************************************")
+print("El salario bruto es de : ", salrbrut)
+print("el descuento total es de: ", desctotal)
+print("el salario total es de: ",salrbrut )
+print("***********************************************")
+  
