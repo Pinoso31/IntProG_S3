ae6563f6a1dfdee459265d423ebe6ce1ed46a4a1
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-04-03 18:23:44 -0600

    ejercicio 4 semana 3

---
+++ calculo_de_viajes_con_escalas.py
@@ -0,0 +1,7 @@
+tramo1 = float(input("ingrese la duracion del primer tramo de vuelo"))
+escala1 = float(input("ingrese la duracion la primer escala de vuelo"))
+tramo2 = float(input("Ingresa la duracion del segundo tramo de vuelo"))
+escala2 = float(input("Ingresa la duracion de la segunda escala de vuelo"))
+tramo3 = float(input("Ingresa la duracion del tercer tramo de vuelo"))
+tiempo_total = tramo1 + escala1 + tramo2 + escala2 + tramo3
+print("Tu tiempo de vuelo total fue de:", tiempo_total)
